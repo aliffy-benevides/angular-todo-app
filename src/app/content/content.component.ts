57d53455b8841b9af506078e3454317b04de5e71
Aliffy Benevides <aliffy.benevides@outlook.com>
2020-01-13 02:25:43 -0300

    Add Breadcrumb Navigation

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-content',
@@ -14,9 +16,30 @@ export class ContentComponent implements OnInit {
 
   @Input() baseRouterLink: string = '';
 
-  constructor() { }
+  routeArray = [];
+  routerLinks = [];
+
+  routeArraySubscription: Subscription;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
+  
+  ngOnChanges() {
+    this.routeArraySubscription = this.route.url.subscribe(urlArray => this.routeArray = urlArray)
+  
+    this.routerLinks = [];
+    this.routeArray.forEach((route, index) => {
+      this.routerLinks.push({
+        path: route.path,
+        isLast: index === this.routeArray.length - 1,
+        routerLink: ['/', ...this.routeArray.filter((_, i) => i <= index).map(value => value.path)]
+      })
+    })
+  }
 
+  ngOnDestroy() {
+    this.routeArraySubscription && this.routeArraySubscription.unsubscribe();
+  }
 }
